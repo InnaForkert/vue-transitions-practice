5658ef35af67ed8f6625b0f85a304f0d70f5fd91
Inna Forkert <inna.forkert@gmail.com>
2024-01-31 10:39:42 +0200

    final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="home">
-    <Toast v-if="showToast" />
-    <Todos @badValue="triggerToast" />
-  </div>
+ <div class="home">
+  <Toast v-if="showToast" />
+  <Todos @badValue="triggerToast" />
+  <transition>
+   <div v-if="showP"></div>
+  </transition>
+
+  <button @click="showP = !showP">toggle</button>
+ </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Toast from '../components/Toast'
-import Todos from '../components/Todos'
+import { ref } from "vue";
+import Toast from "../components/Toast";
+import Todos from "../components/Todos";
 
 export default {
-  components: { Toast, Todos },
-  setup() {
-    const showToast = ref(false)
+ components: { Toast, Todos },
+ setup() {
+  const showToast = ref(false);
+  const showP = ref(false);
 
-    const triggerToast = () => {
-      showToast.value = true;
-      setTimeout(() => showToast.value = false, 3000)
-    }
+  const triggerToast = () => {
+   showToast.value = true;
+   setTimeout(() => (showToast.value = false), 3000);
+  };
 
-    return { showToast, triggerToast }
-  }
-}
+  return { showToast, triggerToast, showP };
+ },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
